fc5d9d554f0aa5697e03b5e4449e217c7aa7f97b
baranimurthy <barani@dhrti.net>
2017-01-05 13:02:29 +0530

    Adding new project HelloWorld

---
+++ HelloWorld/app/src/main/java/in/codesource/android/getting_started/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package in.codesource.android.getting_started.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
